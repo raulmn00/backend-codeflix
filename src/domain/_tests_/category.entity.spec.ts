cdc24e657c134611271ff8cee2ecc04b8fe1b2e1
raulmn00 <raul.mariaci@gmail.com>
2024-06-28 00:37:44 +0000

    feat: add category entity and change tests runMode to on-demand

---
+++ src/domain/_tests_/category.entity.spec.ts
@@ -0,0 +1,12 @@
+import { Category } from '../category.entity';
+
+describe('Category Entity Unit Tests', () => {
+  test('contructor', () => {
+    const category = new Category({ name: 'movie' });
+    expect(category.category_id).toBeUndefined();
+    expect(category.description).toBeNull();
+    expect(category.is_active).toBeTruthy();
+    expect(category.name).toBe('movie');
+    expect(category.created_at).toBeInstanceOf(Date);
+  });
+});
